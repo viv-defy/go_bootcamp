d316483d7d078e6db0687167ca02c787767b5247
viv-defy <vishnu@MacBook-Pro.local>
2024-01-05 13:04:27 +0530

    split the story 7 and story 8 cases into individual test cases

--- basic_number_filtering/basic_number_filtering_test.go
+++ basic_number_filtering/basic_number_filtering_test.go
@@ -75,7 +75,7 @@ func TestStory6(t *testing.T) {
 	}
 }
 
-func TestStory7(t *testing.T) {
+func TestStory7a(t *testing.T) {
 	conditions := []func(int) bool{isOdd, isGreaterThan(5), isMultipleOf(3)}
 	res := All([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
 	expected := []int{9, 15}
@@ -83,17 +83,19 @@ func TestStory7(t *testing.T) {
 	if !areSlicesEqual(res, expected) {
 		t.Errorf("expected - %v, got - %v", expected, res)
 	}
+}
 
-	conditions = []func(int) bool{isEven, isLessThan(15), isMultipleOf(3)}
-	res = All([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
-	expected = []int{6, 12}
+func TestStory7b(t *testing.T) {
+	conditions := []func(int) bool{isEven, isLessThan(15), isMultipleOf(3)}
+	res := All([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
+	expected := []int{6, 12}
 
 	if !areSlicesEqual(res, expected) {
 		t.Errorf("expected - %v, got - %v", expected, res)
 	}
 }
 
-func TestStory8(t *testing.T) {
+func TestStory8a(t *testing.T) {
 	conditions := []func(int) bool{isPrime, isGreaterThan(15), isMultipleOf(5)}
 	res := Any([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
 	expected := []int{2, 3, 5, 7, 10, 11, 13, 15, 16, 17, 18, 19, 20}
@@ -101,10 +103,12 @@ func TestStory8(t *testing.T) {
 	if !areSlicesEqual(res, expected) {
 		t.Errorf("expected - %v, got - %v", expected, res)
 	}
+}
 
-	conditions = []func(int) bool{isLessThan(6), isMultipleOf(3)}
-	res = Any([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
-	expected = []int{1, 2, 3, 4, 5, 6, 9, 12, 15, 18}
+func TestStory8b(t *testing.T) {
+	conditions := []func(int) bool{isLessThan(6), isMultipleOf(3)}
+	res := Any([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, conditions)
+	expected := []int{1, 2, 3, 4, 5, 6, 9, 12, 15, 18}
 
 	if !areSlicesEqual(res, expected) {
 		t.Errorf("expected - %v, got - %v", expected, res)
