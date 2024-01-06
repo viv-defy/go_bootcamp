fefea848a735a2efad5e3a27660cce12ad9493a0
viv-defy <vishnu@MacBook-Pro.local>
2024-01-06 23:31:22 +0530

    added story3

--- game_of_pig/main.go
+++ game_of_pig/main.go
@@ -58,30 +58,42 @@ func game(p1, p2 player) player {
 	}
 }
 
-func simulateGames(p1Hold, p2Hold int) {
+func simulateGames(p1Hold, p2Hold int) int {
 	p1 := player{id: 1, strategy: p1Hold}
 	p2 := player{id: 2, strategy: p2Hold}
-	var scoreBoard [2]int
+	var wins int
 	for i := 0; i < 10; i++ {
-		winner := game(p1, p2)
-		scoreBoard[winner.id-1]++
+		if winner := game(p1, p2); winner.id == 1 {
+			wins += 1
+		}
 		p1.reset()
 		p2.reset()
 	}
-	fmt.Printf("Holding at  %v vs Holding at  %v: wins: %v/10 (%.1f%%), losses: %v/10 (%.1f%%)\n",
-		p1.strategy, p2.strategy,
-		scoreBoard[0], float64(scoreBoard[0])/10.0*100.0,
-		scoreBoard[1], float64(scoreBoard[1])/10.0*100.0)
+	return wins
 }
 
-func fixStartegies(p1Start, p1End, p2Start, p2End int) {
+func fixStartegies(p1Start int, p1End int, p2Start int, p2End int, printPerStrategy bool) {
+	var wins, winsAti int
 	for i := p1Start; i <= p1End; i++ {
 		for j := p2Start; j <= p2End; j++ {
 			if i == j {
 				continue
 			}
-			simulateGames(i, j)
+			wins = simulateGames(i, j)
+			if printPerStrategy {
+				fmt.Printf("Holding at  %v vs Holding at  %v: wins: %v/10 (%.1f%%), losses: %v/10 (%.1f%%)\n",
+					i, j,
+					wins, float64(wins)/10.0*100.0,
+					10-wins, float64(wins)/10.0*100.0)
+			} else {
+				winsAti += wins
+			}
 		}
+		if !printPerStrategy {
+			fmt.Printf("Result: Wins, losses staying at k = %v: %v/990 (%.1f%%), %v/990 (%.1f%%)\n",
+				i, winsAti, float64(winsAti)/990.0*100.0, 990-winsAti, float64(990-winsAti)/990.0*100.0)
+		}
+		winsAti = 0
 	}
 }
 
@@ -98,7 +110,7 @@ func story1() {
 		return
 	}
 
-	fixStartegies(p1Hold, p1Hold, p2Hold, p2Hold)
+	fixStartegies(p1Hold, p1Hold, p2Hold, p2Hold, true)
 }
 
 func story2() {
@@ -124,7 +136,43 @@ func story2() {
 		return
 	}
 
-	fixStartegies(p1Hold, p1Hold, p2Start, p2End)
+	fixStartegies(p1Hold, p1Hold, p2Start, p2End, true)
+}
+
+func story3() {
+	p1Range := strings.Split(os.Args[1], "-")
+	p1Start, err := strconv.Atoi(p1Range[0])
+	if err != nil {
+		fmt.Println("Invalid argument 1")
+		return
+	}
+	p1End, err := strconv.Atoi(p1Range[1])
+	if err != nil {
+		fmt.Println("Invalid argument 1")
+		return
+	}
+	if p1Start > p1End {
+		fmt.Println("Invalid argument 1")
+		return
+	}
+
+	p2Range := strings.Split(os.Args[2], "-")
+	p2Start, err := strconv.Atoi(p2Range[0])
+	if err != nil {
+		fmt.Println("Invalid argument 2")
+		return
+	}
+	p2End, err := strconv.Atoi(p2Range[1])
+	if err != nil {
+		fmt.Println("Invalid argument 2")
+		return
+	}
+	if p2Start > p2End {
+		fmt.Println("Invalid argument 2")
+		return
+	}
+
+	fixStartegies(p1Start, p1End, p2Start, p2End, false)
 }
 
 func main() {
@@ -135,4 +183,5 @@ func main() {
 
 	story1()
 	story2()
+	story3()
 }
